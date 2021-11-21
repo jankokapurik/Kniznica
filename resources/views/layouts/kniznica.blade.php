5af20c2340457f99f2f136c02a2fcfec98159b1a
Jan Kapurik <jankokapurik@gmail.com>
2021-11-21 07:42:54 +0000

    search

--- resources/views/layouts/kniznica.blade.php
+++ resources/views/layouts/kniznica.blade.php
@@ -22,6 +22,7 @@
         </ul>
         <div>
             <form action="" method="post" class="flex items-center">
+                @csrf
                 <label for="search" class="sr-only">Name</label>
                 <input type="text" name="search" placeholder="Vyhľadaj" class="bg-gray-100 border-2 w-full p-1 rounded-lg mr-2">
                 <button type="submit" class="bg-blue-500 text-white p-1 rounded font-medium  hover:opacity-50">Hľadaj</button>
