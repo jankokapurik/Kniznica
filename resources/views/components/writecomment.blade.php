f105549c38e95900cd8a6dba53b7ffc80eb14ff4
pavolKe <93823407+pavolKe@users.noreply.github.com>
2021-11-30 19:51:55 +0100

    Rating zaklad

--- resources/views/components/writecomment.blade.php
+++ resources/views/components/writecomment.blade.php
@@ -5,9 +5,17 @@
         @csrf
         <input type="hidden" name="book" value="{{ $book->id }}" />
 
-        <div class="mb-4">
+        <div class="mb-4 bg-white">
+            <label for="rating"></label>
+            <input type="number" name="rating" min="1" max="5" step="1" value="1">
+            @error('rating')
+                <div class="text-red-500 mt-2 text-sm">
+                    {{ $message }}
+                </div>
+            @enderror
             <label for="body" class="sr-only">Body</label>
-            <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 vorder-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror" placeholder="Post something!" ></textarea>
+            <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 vorder-2 w-full p-4 rounded-lg 
+            @error('body') border-red-500 @enderror" placeholder="Post something!"></textarea>
     
             @error('body')
                 <div class="text-red-500 mt-2 text-sm">
